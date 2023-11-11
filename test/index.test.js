c3daeae2f4d088a37724d65e9ec615577f14dabe
sedwinj <s3782699@student.rmit.edu.au>
2023-11-11 15:41:00 +1100

    Streamline the test process and simplify additions

--- test/index.test.js
+++ test/index.test.js
@@ -1,22 +1,39 @@
 import { md2html } from "../src"
 
+function pair(input, expected) {
+    input = md2html.parse(input).innerHTML
+    expected = new DOMParser().parseFromString(expected, 'text/html').body.innerHTML
+    return { input, expected }
+}
+
 describe('MdToHtml', () => {
-
     it('can convert Markdown headings to HTML headings', () => {
-        // Headers 1 through 6
-        expect(md2html.parse('# Header').innerHTML).toEqual('<h1>Header</h1>')
-        expect(md2html.parse('## Header').innerHTML).toEqual('<h2>Header</h2>')
-        expect(md2html.parse('### Header').innerHTML).toEqual('<h3>Header</h3>')
-        expect(md2html.parse('#### Header').innerHTML).toEqual('<h4>Header</h4>')
-        expect(md2html.parse('##### Header').innerHTML).toEqual('<h5>Header</h5>')
-        expect(md2html.parse('###### Header').innerHTML).toEqual('<h6>Header</h6>')
+        let equal = [
+            // Headers 1 through 6
+            pair('# Header', '<h1>Header</h1>'),
+            pair('## Header', '<h2>Header</h2>'),
+            pair('### Header', '<h3>Header</h3>'),
+            pair('#### Header', '<h4>Header</h4>'),
+            pair('##### Header', '<h5>Header</h5>'),
+            pair('###### Header', '<h6>Header</h6>'),
+            // Alternate headings
+            pair('Header\n======', '<h1>Header</h1>'),
+            pair('Header\n------', '<h2>Header</h2>')
+        ]
 
-        // No headers beyond level 6
-        expect(['<h6>Header</h6>', '<h7>Header</h7>']).not.toContain(md2html.parse('####### Header').innerHTML)
+        for (let pair of equal) {
+            expect(pair.input).toEqual(pair.expected)
+        }
 
-        // // Alternate headings
-        expect(md2html.parse('Header\n======').innerHTML).toEqual('<h1>Header</h1>')
-        expect(md2html.parse('Header\n------').innerHTML).toEqual('<h2>Header</h2>')
+        let notEqual = [
+            // No headers beyond level 6
+            pair('####### Header', '<h6>Header</h6>'),
+            pair('####### Header', '<h7>Header</h7>')
+        ]
+
+        for (let pair of notEqual) {
+            expect(pair.input).not.toEqual(pair.expected)
+        }
     })
 
     it.todo('can convert Markdown paragraphs into HTML paragraphs')
